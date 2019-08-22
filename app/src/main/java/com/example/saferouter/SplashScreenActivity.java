687a866fb0caa545d1d9e765bb1556ef8cb1549e
Francis <flin0002@student.monash.edu>
2019-08-22 16:06:37 +1000

    implement map, search location, display destination and draw polyline

--- app/src/main/java/com/example/saferouter/SplashScreenActivity.java
+++ app/src/main/java/com/example/saferouter/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.saferouter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,29 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(1000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(SplashScreenActivity.this, MapActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
     }
 }
