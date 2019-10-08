c38bdc455e6d93623234a1a49caa24e134ba3285
Francis <flin0002@student.monash.edu>
2019-10-08 15:47:45 +1100

    cut dangerous messages in the toast to be more clear

--- app/src/main/java/com/example/saferouter/MapActivity.java
+++ app/src/main/java/com/example/saferouter/MapActivity.java
@@ -726,6 +726,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             double distanceBetweenTwoConsecutivePoints = calculateDistanceBetweenTwoPoint(dangerousPoint, nextPoint);
             if (distanceBetweenClickedAndDangerousPoint + distanceBetweenClickedAndNextPoint - distanceBetweenTwoConsecutivePoints < 50) {
                 String message = voiceMessagesListOfRoutes.get(selectedRouteNo).get(dangerousPointIndex);
+                message = StringUtils.substringBefore(message, " AT");
                 Toast.makeText(MapActivity.this, message, Toast.LENGTH_LONG).show();
             }
 
